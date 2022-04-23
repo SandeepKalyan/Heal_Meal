4348da05249fd129b04853edf189b57d2b7d49f2
Sandeep Kalyan Sunkara <vvsvsk.1201@gmail.com>
2022-04-23 19:24:14 -0400

    Backend: Updated user models

--- Backend/models/user.go
+++ Backend/models/user.go
@@ -1,8 +1,12 @@
 package models
 
+import "gorm.io/datatypes"
+
 type User struct{
 	ID uint `json:"id"`
-	Name string `json:"name"`
+	FirstName string `json:"firstname"`
+	LastName string `json:"lastname"`
 	Email string `json:"email" gorm:"unique"`
 	Password []byte `json:"password"`
+	Cart datatypes.JSON
 }
