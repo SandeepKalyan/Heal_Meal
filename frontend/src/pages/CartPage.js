a6f964c1a3239d6606145f1d27871f2a6ddea7a3
Sandeep Kalyan Sunkara <vvsvsk.1201@gmail.com>
2022-04-25 00:02:40 -0400

    Frontend & Backend: Created cart page and integrated with backend.

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,9 +1,35 @@
 import Head from "../components/Head"
+import CartCards from "../components/CartCards"
+import { useState } from "react";
+import { useEffect } from "react"
 function CartPage() {
-    return (
+    const [json, setJson] = useState();
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch(
+                    'http://localhost:8000/api/cart', {
+                    headers: { "Content": "application/json" },
+                    credentials: "include"
+                });
+                const content = await response.json();
+                setJson(content)
+            }
+        )();
+    });
 
+    return (
         <div>
             <Head />
+            <h2>Check out below items in your cart</h2>
+            <CartCards json={json} />
+            <button
+                type="button"
+                onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = '/home';
+                }}
+            > Click here</button>
         </div>
     )
 }
