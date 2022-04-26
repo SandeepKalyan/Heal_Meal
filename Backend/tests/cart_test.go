8c3b94035dde904fe69bf8eaf0d7f9f683d7621e
Sandeep Kalyan Sunkara <vvsvsk.1201@gmail.com>
2022-04-26 02:03:39 -0400

    Backend: Testing Update

--- Backend/tests/cart_test.go
+++ Backend/tests/cart_test.go
@@ -100,3 +100,84 @@ payload2 := strings.NewReader(`{
 		})
 	}
 }
+
+func TestClearCart(t *testing.T) {
+
+	url := "http://127.0.0.1:8000/api/login"
+	method := "POST"
+
+	payload := strings.NewReader(`{
+    "email":"a@aasbvsf",
+    "password":"asdbvfd"
+}`)
+client := &http.Client {
+}
+req, err := http.NewRequest(method, url, payload)
+
+if err != nil {
+  fmt.Println(err)
+  return
+}
+req.Header.Add("Content-Type", "application/json")
+
+res, err := client.Do(req)
+if err != nil {
+  fmt.Println(err)
+  return
+}
+defer res.Body.Close()
+
+body , err := ioutil.ReadAll(res.Body)
+if err != nil {
+  fmt.Println(err)
+  return
+}
+
+fmt.Println(string(body))
+urlnew := "http://127.0.0.1:8000/api/clearcart"
+methodnew := "POST"
+
+
+	tests := []struct {
+		name    string
+		statuscode int
+	}{
+		{
+			"Valid clear cart",
+			200,
+		},
+		
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client := &http.Client {
+			}
+			req, err := http.NewRequest(methodnew, urlnew, nil)
+		  
+			if err != nil {
+			  fmt.Println(err)
+			  return
+			}
+			req.Header.Add("Content-Type", "application/json")
+			req.Header.Add("Cookie", "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTEwMzc1NTMsImlzcyI6IjM5In0._0zuMlQdWU4HucsavGKkFdlx68GQQdxEz4pMZf1ubyk")
+			res, err := client.Do(req)
+			if err != nil {
+			  fmt.Println(err)
+			  return
+			}
+			defer res.Body.Close()
+		  
+			body , err := ioutil.ReadAll(res.Body)
+			if err != nil {
+			  fmt.Println(err)
+			  return
+			}
+
+			fmt.Println(string(body))
+
+			if res.StatusCode != tt.statuscode {
+				t.Fail()
+			}
+		})
+	}
+}
